e819a2edb9ecd074764f990aca73d7cca4990ad9
sh0nnie4 <139727327+sh0nnie4@users.noreply.github.com>
2024-02-03 19:33:29 -0500

    Updated bootstrap link

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ export default function App() {
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
-      hello world
     </div>
   );
 }
