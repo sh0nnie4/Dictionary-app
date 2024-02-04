0b552d5faba0f4e70abf9ab17c59db01dd493c1f
sh0nnie4 <139727327+sh0nnie4@users.noreply.github.com>
2024-02-04 13:28:54 -0500

    Added search form and dictionary componenet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import logo from "./logo2.jpg";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Shontay Arrington</small>
+        </footer>
+      </div>
     </div>
   );
 }
